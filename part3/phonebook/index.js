e712413efb1a5dbec2fdcf9130cae6b6e67ec384
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-11-12 17:21:18 +0530

    :rotating_light: chore(phonebook): fixed lint warnings

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,152 +1,153 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
-const middleware = require("./middleware");
-const phonebook = require("./mongo");
+const middleware = require('./middleware')
+const phonebook = require('./mongo')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static("build"));
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 // morgan token for logging the body
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+// eslint-disable-next-line no-unused-vars
+morgan.token('body', (req, _) => JSON.stringify(req.body))
 app.use(
-	morgan(
-		":method :url :status :res[content-length] - :response-time ms :body",
-	),
-);
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body',
+  ),
+)
 
 const validation = async (req, res) => {
-	const { name, number } = req.body;
-	if (!name || !number) {
-		res.status(400).json({
-			error: "name or number missing",
-		});
-		return false;
-	}
+  const { name, number } = req.body
+  if (!name || !number) {
+    res.status(400).json({
+      error: 'name or number missing',
+    })
+    return false
+  }
 
-	const nameRegex = new RegExp("^[a-zA-Z ]+$");
-	if (!nameRegex.test(name)) {
-		res.status(400).json({
-			error: "name must contain only letters and spaces",
-		});
-		return false;
-	}
+  const nameRegex = new RegExp('^[a-zA-Z ]+$')
+  if (!nameRegex.test(name)) {
+    res.status(400).json({
+      error: 'name must contain only letters and spaces',
+    })
+    return false
+  }
 
-	const numberRegex = new RegExp("^[0-9]+$");
-	if (!numberRegex.test(number)) {
-		res.status(400).json({
-			error: "number must contain only numbers",
-		});
-		return false;
-	}
+  const numberRegex = new RegExp('^[0-9]+$')
+  if (!numberRegex.test(number)) {
+    res.status(400).json({
+      error: 'number must contain only numbers',
+    })
+    return false
+  }
 
-	const searchPerson = await phonebook.find({ number });
-	if (searchPerson.length > 0) {
-		res.status(400).json({
-			error: "number must be unique",
-		});
-		return false;
-	}
+  const searchPerson = await phonebook.find({ number })
+  if (searchPerson.length > 0) {
+    res.status(400).json({
+      error: 'number must be unique',
+    })
+    return false
+  }
 
-	return true;
-};
+  return true
+}
 
 const formatPhonebook = (phonebook) => {
-	return {
-		id: phonebook._id,
-		name: phonebook.name,
-		number: phonebook.number,
-	};
-};
+  return {
+    id: phonebook._id,
+    name: phonebook.name,
+    number: phonebook.number,
+  }
+}
 
-app.get("/", (_, res) => {
-	res.send("Welcome to phonebook app!");
-});
+app.get('/', (_, res) => {
+  res.send('Welcome to phonebook app!')
+})
 
-app.get("/info", async (_, res) => {
-	const count = await phonebook.countDocuments();
-	const date = new Date();
+app.get('/info', async (_, res) => {
+  const count = await phonebook.countDocuments()
+  const date = new Date()
 
-	res.send(`
+  res.send(`
 		<p>Phonebook has info for ${count} people</p>
 		<p>${date}</p>
-	`);
-});
+	`)
+})
 
-app.get("/api/persons", async (_, res) => {
-	try {
-		const persons = await phonebook.find({});
-		res.json(persons.map(formatPhonebook));
-	} catch (error) {
-		console.log(error);
-	}
-});
+app.get('/api/persons', async (_, res) => {
+  try {
+    const persons = await phonebook.find({})
+    res.json(persons.map(formatPhonebook))
+  } catch (error) {
+    console.log(error)
+  }
+})
 
-app.post("/api/persons", async (req, res, next) => {
-	try {
-		const isValid = await validation(req, res);
-		if (isValid) {
-			const person = new phonebook({
-				name: req.body.name,
-				number: req.body.number,
-			});
-			const savedPerson = await person.save();
-			res.json(formatPhonebook(savedPerson));
-		}
-	} catch (error) {
-		next(error);
-	}
-});
+app.post('/api/persons', async (req, res, next) => {
+  try {
+    const isValid = await validation(req, res)
+    if (isValid) {
+      const person = new phonebook({
+        name: req.body.name,
+        number: req.body.number,
+      })
+      const savedPerson = await person.save()
+      res.json(formatPhonebook(savedPerson))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
-app.get("/api/persons/:id", async (req, res, next) => {
-	try {
-		const person = await phonebook.findById(req.params.id);
-		if (person) {
-			res.json(formatPhonebook(person));
-		} else {
-			res.status(404).end();
-		}
-	} catch (error) {
-		next(error);
-	}
-});
+app.get('/api/persons/:id', async (req, res, next) => {
+  try {
+    const person = await phonebook.findById(req.params.id)
+    if (person) {
+      res.json(formatPhonebook(person))
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
-app.put("/api/persons/:id", async (req, res, next) => {
-	try {
-		const isValid = await validation(req, res);
-		if (isValid) {
-			const person = {
-				name: req.body.name,
-				number: req.body.number,
-			};
-			const updatedPerson = await phonebook.findByIdAndUpdate(
-				req.params.id,
-				person,
-				{ new: true },
-			);
-			res.json(formatPhonebook(updatedPerson));
-		}
-	} catch (error) {
-		next(error);
-	}
-});
+app.put('/api/persons/:id', async (req, res, next) => {
+  try {
+    const isValid = await validation(req, res)
+    if (isValid) {
+      const person = {
+        name: req.body.name,
+        number: req.body.number,
+      }
+      const updatedPerson = await phonebook.findByIdAndUpdate(
+        req.params.id,
+        person,
+        { new: true },
+      )
+      res.json(formatPhonebook(updatedPerson))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
-app.delete("/api/persons/:id", async (req, res, next) => {
-	try {
-		await phonebook.findByIdAndRemove(req.params.id);
-		res.status(204).end();
-	} catch (error) {
-		next(error);
-	}
-});
+app.delete('/api/persons/:id', async (req, res, next) => {
+  try {
+    await phonebook.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server started on port ${process.env.PORT}`);
-});
+  console.log(`Server started on port ${process.env.PORT}`)
+})
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
 
 const phonebookSchema = new mongoose.Schema({
-	name: String,
-	number: String,
-});
+  name: String,
+  number: String,
+})
 
-phonebookSchema.set("toJSON", {
-	transform: (_, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
+phonebookSchema.set('toJSON', {
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 });
 
 (async () => {
-	try {
-		await mongoose.connect(url);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log(error);
-	}
-})();
+  try {
+    await mongoose.connect(url)
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.log(error)
+  }
+})()
 
-module.exports = mongoose.model("Phonebook", phonebookSchema);
+module.exports = mongoose.model('Phonebook', phonebookSchema)
